20fcfb9345520856f07d2b41ec4dbd91d8de0178
Abu Taher Molla <57549941+AbuTaherMolla24@users.noreply.github.com>
2020-08-22 09:39:45 +0600

    32.7 try

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const naioks = ['anowar', 'salman', 'jafar']
   return (
     <div className="App">
       <header className="App-header">
        <p>I am react person kk</p>
-       <Person></Person>
-       <Person></Person>
-       <Person></Person>
+       <Person name= {naioks[0]} naika='tuntuni'></Person>
+       <Person name= 'jahim' naika='shabana'></Person>
+       <Person name="bapparaz" naika="shabana"></Person>
       </header>
     </div>
   );
 }
-function Person(){
+function Person(props){
   const personStyle={
     border: '5px solid gray',
     margin: '10px'
   }
+  console.log(props)
   return (
   <div style= {personStyle}>
-    <h1>Name: Tuntuni</h1>
-    <h3>Hero of the year</h3>
+    <h1>Name: {props.name} </h1>
+    <h3>Hero of {props.naika}</h3>
   </div>
   )
 }
